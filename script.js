559b7beceaebdca7107fc07a8f0999f9175d5626
Sebastien Dutripon <seb032@live.fr>
2017-01-10 10:50:57 +0100

    Fin du HTML et CSS et début Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function NombresdeCases(){
+	var nombres = ;
+	var nbDeTours, compteur ;
+	var question = "Donne moi le nombre de case que tu veut?";
+	var tableau = New Array ;
+	var nbdeCases = ;
+
+nbDeTours = 1;
+		while nbDeTours < nbdeCases;
+		 tableau.push(parseInt(prompt("Donne moi le nombres de case que tu veut?")));
