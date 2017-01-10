053a1203506f824804366919cef4bc1d58f0fdba
Sebastien Dutripon <seb032@live.fr>
2017-01-10 16:05:26 +0100

    Ajout de la detection du javascript

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var seuil;
+var seuil
 var i;
 
-var seuil= prompt("Combien veut tu de case ? Entre 1 et 200")
+var seuil= prompt("Combien veut tu de case ? Entre 1 et 1000")
+  while(seuil > 1000){
+    var seuil=prompt("Choisit un nombre entre 1 et 1000")
+    }
 for (var i = 1; i <= seuil ; i++) {
     document.getElementById('case').innerHTML += "<li>" +i+ "</li>";
+
 }
